e9bfcc0d583152f6a37f9c10e68a0edada0c8206
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-03-18 13:56:15 +0300

    side_tasks eklendi. persistance eklendi (store in local file). Testler başlandı

---
+++ telegram_bot/event_handler/coin_message_sender.py
@@ -0,0 +1,66 @@
+import sys
+import os
+
+# Add the root directory of your project to the Python path to import from parent directory
+# to fix configs import problem
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from configs import *
+from telegram import Bot
+import asyncio
+import pika
+import json
+
+bot = Bot('7192726917:AAHbXfJlu6dgb2IhdVTtozzQ1CM6t8tfcBo')
+
+# asyncio.run(consume_messages())
+
+
+async def consume_gas():
+    ...
+
+
+async def send_mes(data):
+    # reassign bot for preventing error after object change
+    bot = Bot('7192726917:AAHbXfJlu6dgb2IhdVTtozzQ1CM6t8tfcBo')
+    print('sending')
+
+    # create send text
+    text = data['coin'] + 'change rate: ' + data['rate']
+    # Assuming bot is an asynchronous object that can send messages
+    await bot.send_message(chat_id=1359422473, text=text)
+    print('message sent')
+
+
+# coin data sending function
+def coin_event(ch, method, properties, body: bytes):
+    print('event')
+    try:
+        data = json.loads(body.decode("utf-8"))
+        asyncio.run(send_mes(data))  # Run the asynchronous function with an event loop
+        print("Task created for:", data)
+    except Exception as e:
+        print("Error processing message:", e)
+
+
+def consume_coin():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=COIN_QUEUE_MINUTE_BASED)
+
+    channel.basic_consume(queue=COIN_QUEUE_MINUTE_BASED, on_message_callback=coin_event, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+
+    channel.start_consuming()
+
+
+consume_coin()
+
+
+async def consume_funding():
+    ...
+
+
+# asyncio.run(consume_coin())
