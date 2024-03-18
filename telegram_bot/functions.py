bda29b0a204b3de91c5abba93b0b7d73e1c5684b
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-03-18 14:01:42 +0300

    fonksiyon eklendi ve tracker ile bağlantısı düzeltildi

--- telegram_bot/functions.py
+++ telegram_bot/functions.py
@@ -1,6 +1,6 @@
+import json
 import logging
 from typing import Dict
-from consumer import *
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, Bot
 from telegram.ext import (
     Application,
@@ -10,14 +10,20 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
+from telegram.ext import PicklePersistence
+# local imports
+from menu_keyboards import *
+from changable_configs import *
+from key_configs import *
+
+my_persistence = PicklePersistence(filepath='persistence')
 
 # storage for testing -> get to db later on
 tracked_coins = []
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 # set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
@@ -25,17 +31,15 @@ logger = logging.getLogger(__name__)
 
 CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
 
-reply_keyboard = [
-    ["Track 🐾", "My Tracks 👞"],
-    ["NFT 🖼️", "Pool 🏊", "Gas Price 🚰", "Funding 💰"],
-    ["Main Menu 📑"],
-]
-
+# different from inlinekeyboardmarkup not inline but reply
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 
-async def message_sender():
-    await bot.send_message(chat_id=1359422473, text="asd")
+def store_coin(coin_name):
+    json_file = open('coins.json', 'r')
+    coin_data = json_file.read()
+    coin_data = json.loads(coin_data)
+    json_file.close()
 
 
 def facts_to_str(user_data: Dict[str, str]) -> str:
@@ -46,46 +50,34 @@ def facts_to_str(user_data: Dict[str, str]) -> str:
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Start the conversation and ask user for input."""
+
     await update.message.reply_text(
-        "Your document sending process has been completed successfully.",
-        reply_markup=markup,
+        START_RETURN_TEXT,
+        # reply_markup=main_menu_keyboard(),
     )
     # await bot.send_message(chat_id=1359422473, text="asd")
 
-    return CHOOSING
+    return  CHOOSING # main_menu_keyboard()
 
 
 # after choosing an option, this function runs
 async def select_nft(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
-    global tracked_coins
-
     text = update.message.text
     chat_id = update.message.chat_id
 
     context.user_data["choice"] = text
-    await update.message.reply_text(f"chat id: {chat_id}")
-
-    tracked_coins.append(text.lower())
-
-    print(tracked_coins)
+    await update.message.reply_text(NFT_RETURN_TEXT)
 
     return TYPING_REPLY
-
 
 # pool selection
 async def select_pool(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
-    global tracked_coins
-
     text = update.message.text
-    print("pool selectedd")
+
     context.user_data["choice"] = text
-    await update.message.reply_text(f"Your {text.lower()}? Yes, I would love to hear about that!")
-
-    tracked_coins.append(text.lower())
-
-    print(tracked_coins)
+    await update.message.reply_text(POOL_RETURN_TEXT)
 
     return TYPING_REPLY
 
@@ -93,16 +85,9 @@ async def select_pool(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int
 # gas price selection
 async def select_gas(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
-    global tracked_coins
-
-    print("gas selected")
     text = update.message.text
     context.user_data["choice"] = text
-    await update.message.reply_text(f"Your {text.lower()}? Yes, I would love to hear about that!")
-
-    tracked_coins.append(text.lower())
-
-    print(tracked_coins)
+    await update.message.reply_text(GAS_PRICE_RETURN_TEXT, reply_markup=add_menu_keyboard())
 
     return TYPING_REPLY
 
@@ -110,16 +95,10 @@ async def select_gas(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 # track selection
 async def select_track(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
-    global tracked_coins
-
-    print("track selected")
     text = update.message.text
     context.user_data["choice"] = text
-    await update.message.reply_text(f"Your {text.lower()}? Yes, I would love to hear about that!")
 
-    tracked_coins.append(text.lower())
-
-    print(tracked_coins)
+    await update.message.reply_text(TRACKER_RETURN_TEXT, reply_markup=track_keyboard())
 
     return TYPING_REPLY
 
@@ -127,32 +106,18 @@ async def select_track(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
 # show my tracks
 async def show_my_tracks(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
-    global tracked_coins
-
-    print("show my tracks selected")
     text = update.message.text
     context.user_data["choice"] = text
-    await update.message.reply_text(f"Your {text.lower()}? Yes, I would love to hear about that!")
-
-    tracked_coins.append(text.lower())
-
-    print(tracked_coins)
+    await update.message.reply_text(MY_TRACKS_RETURN_TEXT)
 
     return TYPING_REPLY
 
 
 async def select_funding(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
-    global tracked_coins
-
-    print("funding selected")
     text = update.message.text
     context.user_data["choice"] = text
-    await update.message.reply_text(f"Your {text.lower()}? Yes, I would love to hear about that!")
-
-    tracked_coins.append(text.lower())
-
-    print(tracked_coins)
+    await update.message.reply_text(FUNDING_RETURN_TEXT)
 
     return TYPING_REPLY
 
@@ -160,9 +125,6 @@ async def select_funding(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 # over selection
 async def selection_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
-    global tracked_coins
-
-    print("selections selected")
     text = update.message.text
     context.user_data["choice"] = text
     await update.message.reply_text(f"Your {text.lower()}? Yes, I would love to hear about that!")
@@ -186,11 +148,16 @@ async def custom_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
 async def received_information(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Store info provided by user and ask for the next category."""
     user_data = context.user_data
+
     text = update.message.text
     print(text)
     category = user_data["choice"]
     user_data[category] = text
     del user_data["choice"]
+
+    json_file = open('darasave.json', 'w')
+    json_file.write(json.dumps(user_data))
+    json_file.close()
 
     await update.message.reply_text(
         "Neat! Just so you know, this is what you already told me:"
@@ -204,16 +171,10 @@ async def received_information(update: Update, context: ContextTypes.DEFAULT_TYP
 
 async def go_main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
-    global tracked_coins
-
-    print("main menu selected")
     text = update.message.text
     context.user_data["choice"] = text
-    await update.message.reply_text(f"Your {text.lower()}? Yes, I would love to hear about that!")
 
-    tracked_coins.append(text.lower())
-
-    print(tracked_coins)
+    await update.message.reply_text(MAIN_MENU_BUTTON_TEXT, reply_markup=main_menu_keyboard())
 
     return TYPING_REPLY
 
