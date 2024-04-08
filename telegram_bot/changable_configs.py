938204ad9954cdd5ed7b03ba8d6f3768403b7238
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-04-08 15:00:52 +0300

    Update changable_configs.py

--- telegram_bot/changable_configs.py
+++ telegram_bot/changable_configs.py
@@ -6,10 +6,10 @@ reply_keyboard = [
 
 # RETURN TEXTS, Don't leave blank it creates error
 START_RETURN_TEXT = "Greetings, let's make some money together! "
-TRACKER_RETURN_TEXT = "a"
-NFT_RETURN_TEXT = "a"
+TRACKER_RETURN_TEXT = "Please enter a coin name: "
+NFT_RETURN_TEXT = "Please enter NFT name..."
 FUNDING_RETURN_TEXT = "a"
 POOL_RETURN_TEXT = "a"
 GAS_PRICE_RETURN_TEXT = "a"
 MY_TRACKS_RETURN_TEXT = "a"
-MAIN_MENU_BUTTON_TEXT= "a"
+MAIN_MENU_BUTTON_TEXT= "Main Menu"
