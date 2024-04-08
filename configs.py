e7306c6458959ade3f33ffaee1eed5bb0a064db0
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-04-08 15:06:47 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,9 +1,17 @@
 from typing import Final
+import os
 
+# RabbitMQ queue names, not very important, changeable because all authorities get from here
 COIN_QUEUE_FLAGGED: Final = "coin change flag event"
 COIN_QUEUE_MINUTE_BASED: Final = "coin change min event"
 NFT_QUEUE: Final = "nft event"
 GAS_QUEUE: Final = "gas event"
 FUNDING_QUEUE: Final = "funding event"
-QUEUES: Final = ["coin change flag event", "coin change min event", "nft event", "gas event", "funding event"]
+QUEUES: Final = [COIN_QUEUE_FLAGGED, COIN_QUEUE_MINUTE_BASED, NFT_QUEUE, GAS_QUEUE, FUNDING_QUEUE]
 
+# bot key, you can create new bot key using BotFather telegram
+BOT_KEY: Final = '7192726917:AAHbXfJlu6dgb2IhdVTtozzQ1CM6t8tfcBo'
+
+# coins path
+ALL_COINS_PATH: Final = os.getcwd() + "/coins.json"
+FOLLOWS_PATH: Final = os.getcwd() + "/coin_follows.json"
