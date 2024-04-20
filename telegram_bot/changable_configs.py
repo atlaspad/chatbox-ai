b19a1bf8d972364b77e609844644076adde15630
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-04-21 00:31:19 +0300

    adding wallet and tracking nft

--- telegram_bot/changable_configs.py
+++ telegram_bot/changable_configs.py
@@ -6,10 +6,13 @@ reply_keyboard = [
 
 # RETURN TEXTS, Don't leave blank it creates error
 START_RETURN_TEXT = "Greetings, let's make some money together! "
-TRACKER_RETURN_TEXT = "Please enter a coin name: "
-NFT_RETURN_TEXT = "Please enter NFT name..."
+TRACKER_RETURN_TEXT = "Lütfen bir coin ismi giriniz: "
+NFT_RETURN_TEXT = "Lütfen NFT koleksiyonu giriniz..."
 FUNDING_RETURN_TEXT = "a"
 POOL_RETURN_TEXT = "a"
 GAS_PRICE_RETURN_TEXT = "a"
 MY_TRACKS_RETURN_TEXT = "a"
-MAIN_MENU_BUTTON_TEXT= "Main Menu"
+MAIN_MENU_BUTTON_TEXT= "How to use bot?\nTwitter: https://twitter.com/Atlaspad\nWeb site and FAQ: www.google.com\nYoutube Tutorial: www.youtube.com\nDetailed Guide: www.detail.com"
+ADD_RETURN_TEXT = "Lutfen eklemek icin bir sey seciniz"
+NFT_ADD_RETURN_TEXT = "NFT basari ile eklendi"
+WALLET_ADD_RETURN_TEXT = "Wallet basari ile eklendi"
