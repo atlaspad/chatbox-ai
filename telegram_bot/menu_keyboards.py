1fe5ac36657440c6c7ede8465e3ed5a2b8d9f0d1
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-04-21 00:36:27 +0300

    menu has been revised and made fully functional

--- telegram_bot/menu_keyboards.py
+++ telegram_bot/menu_keyboards.py
@@ -2,11 +2,21 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def main_menu_keyboard():
-    # main menu buttons
-  keyboard = [[InlineKeyboardButton('Menu 1', callback_data='m1')],
-              [InlineKeyboardButton('Menu 2', callback_data='m2')],
-              [InlineKeyboardButton('Menu 3', callback_data='m3')]]
-  return InlineKeyboardMarkup(keyboard)
+  # main menu buttons
+    keyboard = [[InlineKeyboardButton('add ➕', callback_data='menu_add'), InlineKeyboardButton('edit 📝', callback_data='menu_edit')],
+              [InlineKeyboardButton('Who is 👤', callback_data='menu_who'), InlineKeyboardButton('profiles 😎', callback_data='menu_profile')],
+              [InlineKeyboardButton('Settings ⚙️', callback_data='menu_settings'), InlineKeyboardButton('subscription 🎟️', callback_data='menu_sub')]]
+
+    return InlineKeyboardMarkup(keyboard)
+
+
+def add_keyboard():
+  # main menu buttons
+    keyboard = [[InlineKeyboardButton('coin ₿', callback_data='adder_coin'), InlineKeyboardButton('NFT 🖼️', callback_data='adder_nft')],
+              [InlineKeyboardButton('Wallet 💼', callback_data='adder_wallet'), InlineKeyboardButton('Gas Alert 🚰', callback_data='adder_gas')],
+              [InlineKeyboardButton('Funding alert️ 💰', callback_data='adder_funding')]]
+
+    return InlineKeyboardMarkup(keyboard)
 
 
 def track_keyboard():
@@ -17,7 +27,7 @@ def track_keyboard():
 
 def add_menu_keyboard():
     # main menu buttons
-  keyboard = [[InlineKeyboardButton('Menu 1', callback_data='m1'), InlineKeyboardButton('Menu 1', callback_data='m1')],
+    keyboard = [[InlineKeyboardButton('Menu 1', callback_data='m1'), InlineKeyboardButton('Menu 1', callback_data='m1')],
               [InlineKeyboardButton('Menu 2', callback_data='m2'), InlineKeyboardButton('Menu 1', callback_data='m1')],
               [InlineKeyboardButton('Menu 3', callback_data='m3'), InlineKeyboardButton('Menu 1', callback_data='m1')]]
-  return InlineKeyboardMarkup(keyboard)
+    return InlineKeyboardMarkup(keyboard)
