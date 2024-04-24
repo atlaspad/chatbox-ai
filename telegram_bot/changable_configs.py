449678caeb318225467678a9e64491a83d051652
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-04-24 18:41:15 +0300

    Update changable_configs.py

--- telegram_bot/changable_configs.py
+++ telegram_bot/changable_configs.py
@@ -1,18 +1,18 @@
 reply_keyboard = [
     ["Track 🐾", "My Tracks 👞","NFT 🖼️"],
-    ["Pool 🏊", "Gas Price 🚰", "Funding 💰"],
+    ["Wallet 💼", "Gas Price 🚰", "Funding 💰"],
     ["Main Menu 📑"],
 ]
 
 # RETURN TEXTS, Don't leave blank it creates error
 START_RETURN_TEXT = "Greetings, let's make some money together! "
-TRACKER_RETURN_TEXT = "Lütfen bir coin ismi giriniz: "
-NFT_RETURN_TEXT = "Lütfen NFT koleksiyonu giriniz..."
-FUNDING_RETURN_TEXT = "a"
-POOL_RETURN_TEXT = "a"
-GAS_PRICE_RETURN_TEXT = "a"
-MY_TRACKS_RETURN_TEXT = "a"
+TRACKER_RETURN_TEXT = "Please input a coin: "
+NFT_RETURN_TEXT = "Please input a NFT collection name: "
+FUNDING_RETURN_TEXT = "Funding On the Way..."
+POOL_RETURN_TEXT = "Coming soon... "
+GAS_PRICE_RETURN_TEXT = "Coming soon... "
+MY_TRACKS_RETURN_TEXT = "Your tracks ::"
 MAIN_MENU_BUTTON_TEXT= "How to use bot?\nTwitter: https://twitter.com/Atlaspad\nWeb site and FAQ: www.google.com\nYoutube Tutorial: www.youtube.com\nDetailed Guide: www.detail.com"
-ADD_RETURN_TEXT = "Lutfen eklemek icin bir sey seciniz"
-NFT_ADD_RETURN_TEXT = "NFT basari ile eklendi"
-WALLET_ADD_RETURN_TEXT = "Wallet basari ile eklendi"
+ADD_RETURN_TEXT = "Please choose something to add: "
+NFT_ADD_RETURN_TEXT = "NFT added successfully"
+WALLET_ADD_RETURN_TEXT = "Wallet added successfully"
